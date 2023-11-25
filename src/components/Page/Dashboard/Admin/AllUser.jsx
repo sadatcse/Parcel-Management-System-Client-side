39594fb7bda969454f29dcf7de7dfeaa06d20f25
Md Sadat Khan <sadatcse@gmail.com>
2023-11-25 10:18:09 +0600

    modal for user role

--- src/components/Page/Dashboard/Admin/AllUser.jsx
+++ src/components/Page/Dashboard/Admin/AllUser.jsx
@@ -1,51 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../Hook/UseAxioSecure";
-import { FaTrashAlt, FaUsers } from 'react-icons/fa';
+import { FaTrashAlt } from 'react-icons/fa';
+import Swal from "sweetalert2";
 
 const AllUser = () => {
     const axiosSecure = useAxiosSecure();
-    const person ='user';
-    const admin ='admin';
-    const man ='deliveryman'
     const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
             return res.data;
         }
-    })
+    });
 
-    const handleMakeAdmin = async (user, role) => {
-        try {
-            const res = await axiosSecure.patch(`/users/${role}/${user._id}`);
+    const [selectedUser, setSelectedUser] = useState(null);
 
-            queryClient.invalidateQueries('users');
-            refetch();
-        } catch (error) {
+    const handleMakeAdmin = (user) => {
+        setSelectedUser(user);
+    };
 
+    const confirmRoleChange = async (selectedRole) => {
+        if (selectedUser) {
+            try {
+                await axiosSecure.patch(`/users/${selectedRole}/${selectedUser._id}`);
+                setSelectedUser(null);
+                refetch();
+            } catch (error) {
+            }
         }
     };
 
     const handleDeleteUser = async (user) => {
         try {
-            const res = await axiosSecure.delete(`/users/${user._id}`);
-            queryClient.invalidateQueries('users');
-            refetch();
+            const result = await Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            });
+    
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/users/${user._id}`);
+                if (res.data.deletedCount > 0) {
+                    refetch();
+                    await Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                }
+            }
         } catch (error) {
-
+            console.error(error);
         }
     };
+
+    
+
     return (
-<div className="p-4">
-    <div className="flex justify-between items-center mb-4">
-        <h2 className="text-3xl font-bold">All Users</h2>
-        <h2 className="text-3xl font-bold">Total Users: {users.length}</h2>
-    </div>
-    <div className="overflow-x-auto">
-        <table className="min-w-full table-auto">
-   
-            <thead>
+        <div className="p-4">
+            <div className="flex justify-between items-center mb-4">
+                <h2 className="text-3xl font-bold">All Users</h2>
+                <h2 className="text-3xl font-bold">Total Users: {users.length}</h2>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="min-w-full table-auto">
+                <thead>
                 <tr className="bg-gray-200">
                     <th className="px-4 py-2"></th>
                     <th className="px-4 py-2">Name</th>
@@ -54,51 +78,78 @@ const AllUser = () => {
                     <th className="px-4 py-2">Action</th>
                 </tr>
             </thead>
-            <tbody>
-                {users.map((user, index) => (
-                    <tr key={user._id} className={index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}>
-                        <td className="border px-4 py-2">{index + 1}</td>
-                        <td className="border px-4 py-2">{user.name}</td>
-                        <td className="border px-4 py-2">{user.email}</td>
-                        <td className="border px-4 py-2">
-                            {user.role === 'admin' ? (
-                                <button
-                                    onClick={() => handleMakeAdmin(user, 'man')}
-                                    className="btn bg-red-500 text-white py-1 px-3 rounded-lg"
-                                >
-                                    Admin
-                                </button>
-                            ) : user.role === 'man' ? (
-                                <button
-                                    onClick={() => handleMakeAdmin(user, 'admin')}
-                                    className="btn bg-orange-500 text-white py-1 px-3 rounded-lg"
-                                >
-                                    Deliveryman
-                                </button>
-                            ) : (
-                                <button
-                                    onClick={() => handleMakeAdmin(user, 'man')}
-                                    className="btn bg-blue-500 text-white py-1 px-3 rounded-lg"
-                                >
-                                    Regular User
-                                </button>
-                            )}
-                        </td>
-                        <td className="border px-4 py-2">
+                    <tbody>
+                        {users.map((user, index) => (
+                            <tr key={user._id} className={index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}>
+                                <td className="border px-4 py-2">{index + 1}</td>
+                                <td className="border px-4 py-2">{user.name}</td>
+                                <td className="border px-4 py-2">{user.email}</td>
+                                <td className="border px-4 py-2">
+                                    {user.role === 'admin' ? (
+                                        <button
+                                            onClick={() => handleMakeAdmin(user)}
+                                            className="btn bg-red-500 text-white py-1 px-3 rounded-lg"
+                                        >
+                                            Admin
+                                        </button>
+                                    ) : user.role === 'deliveryman' ? (
+                                        <button
+                                            onClick={() => handleMakeAdmin(user)}
+                                            className="btn bg-orange-500 text-white py-1 px-3 rounded-lg"
+                                        >
+                                            Deliveryman
+                                        </button>
+                                    ) : (
+                                        <button
+                                            onClick={() => handleMakeAdmin(user)}
+                                            className="btn bg-blue-500 text-white py-1 px-3 rounded-lg"
+                                        >
+                                            Regular User
+                                        </button>
+                                    )}
+                                </td>
+                                <td className="border px-4 py-2">
+                                    <button
+                                        onClick={() => handleDeleteUser(user)}
+                                        className="btn bg-white hover:bg-red-100 py-1 px-3 rounded-lg"
+                                    >
+                                        <FaTrashAlt className="text-red-600" />
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            {/* Modal for role selection */}
+            {selectedUser && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                    <div className="bg-white p-4 rounded-md">
+                        <h2 className="text-xl font-semibold mb-4">Select Role for {selectedUser.name}</h2>
+                        <div className="flex justify-between">
                             <button
-                                onClick={() => handleDeleteUser(user)}
-                                className="btn bg-white hover:bg-red-100 py-1 px-3 rounded-lg"
+                                onClick={() => confirmRoleChange('admin')}
+                                className="btn bg-red-500 text-white py-1 px-3 rounded-lg mr-2"
                             >
-                                <FaTrashAlt className="text-red-600" />
+                                Admin
                             </button>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>
-</div>
-
+                            <button
+                                onClick={() => confirmRoleChange('deliveryman')}
+                                className="btn bg-orange-500 text-white py-1 px-3 rounded-lg mr-2"
+                            >
+                                Deliveryman
+                            </button>
+                            <button
+                                onClick={() => confirmRoleChange('regular user')}
+                                className="btn bg-blue-500 text-white py-1 px-3 rounded-lg"
+                            >
+                                Regular User
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 };
 
